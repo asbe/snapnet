3f2abee917c43d3a874360e71da6b1186385c92d
Asbjørn Berge <asbjorn.berge@sintef.no>
2019-11-19 10:00:52 +0100

    Further edits for compilation on windows

--- semantic3D_utils/setup.py
+++ semantic3D_utils/setup.py
@@ -6,18 +6,19 @@ import numpy
 
 
 pcl_include_dir = "E:/Prosjekter/lib/vcpkg/installed/x64-windows/include/pcl"
-pcl_lib_dir = "E:/Prosjekter/lib/vcpkg/installed/x64-windows/lib"
+lib_dir = "E:/Prosjekter/lib/vcpkg/installed/x64-windows/lib"
 include_dir = "E:/Prosjekter/lib/vcpkg/installed/x64-windows/include"
+vtk_include_dir = "E:/Prosjekter/lib/vcpkg/installed/x64-windows/include"
 
 ext_modules = [Extension(
        "semantic3D",
        sources=["Semantic3D.pyx", "Sem3D.cxx",],  # source file(s)
-       include_dirs=["./", numpy.get_include(),pcl_include_dir, include_dir],
+       include_dirs=["./", numpy.get_include(),pcl_include_dir, include_dir, vtk_include_dir],
        language="c++",
-       library_dirs=[pcl_lib_dir],
-       libraries=["pcl_common_release","pcl_kdtree_release","pcl_features_release","pcl_surface_release","pcl_io_release"],         
+       library_dirs=[lib_dir],
+       libraries=["vtkCommonCore-8.2","pcl_io_release","pcl_search_release","pcl_common_release","pcl_surface_release"],#["pcl_kdtree_release","pcl_features_release","pcl_surface_release","pcl_io_release"],         
        extra_compile_args = [ "/std:c++14", "/openmp",],
-       extra_link_args=["/std:c++14", '/openmp'],
+       extra_link_args=[],
   )]
 
 setup(
